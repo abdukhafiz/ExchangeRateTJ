390822a958579c35b3fe8a245e2b142a382c271a
Abduhafiz <abdukhafiz@gmail.com>
2023-08-31 21:37:57 +0500

    Added name of the currency field

--- app/base/submodels/currency.py
+++ app/base/submodels/currency.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Currency(models.Model):
     id = models.AutoField(primary_key=True)
-    code = models.CharField(max_length=10, null=False)
+    name = models.CharField(max_length=10, null=False)
+    code = models.SmallIntegerField(null=False)
     symbol = models.CharField(max_length=10, null=True)
 
     def __str__(self):
-        return self.code
+        return self.name
